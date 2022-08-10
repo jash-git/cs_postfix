5b54be7dfd02c2e519c7950643c669aa9b395e67
jash.liao <jash.liao@gmail.com>
2022-08-10 14:33:05 +0800

    Create

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+C# 四則運算 VS Stack [PreFix（前序式）/ InFix（中序式）/ PostFix（後序式）] [Github: Postfix Expression]
+
+資料來源: https://github.com/johnsonj561/PostfixArithmetics
---
+++ VS2022_code/PostfixArithmetics/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Justin Johnson
+ * Practicing C# Data Structures
+ * Program uses a stack to translate a infix arithmetic expression into postfix format
+ */ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericStackLibrary;
+using System.Text.RegularExpressions;
+
+namespace PostfixArithmetics {
+    class Program {
+        static void Main(string[] args) {
+
+            //Get input from user
+            Console.WriteLine("Enter Infix Arithmetic Expression");
+            string userInput = Console.ReadLine();
+
+            //generate postfix output
+            string postfixOutput = PostfixEvaluator.convertInfixToPostfix(userInput);
+            Console.WriteLine("\nUser Input (Infix) = " + userInput + "\nPostfix Output = " + postfixOutput);
+
+            Console.WriteLine("\nCalculating Postfix Expression...");
+            string postfixResult = PostfixEvaluator.evaluatePostfixExpression(postfixOutput);
+            Console.WriteLine(postfixOutput + " = " + postfixResult);
+
+            Console.WriteLine("\nPress any key to terminate...");
+            Console.ReadKey();
+        }
+    }
+}
